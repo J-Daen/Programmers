14347f89bd3ce9659a92317208a662f832824073
J-Daen <jdi2040@naver.com>
2022-08-16 19:35:29 +0900

    추가 풀이

--- src/com/myproject/main.java
+++ src/com/myproject/main.java
@@ -16,9 +16,9 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("test");
-		DoublePriorityQueue solution = new DoublePriorityQueue();
-		solution.solution(new String[] {"I 4", "I 3", "I 2", "I 1", "D 1", "D 1", "D -1", "D -1", "I 5", "I 6"});
-		//solution.solution("1D2S#10S");
+		MakeBigNumber solution = new MakeBigNumber();
+		//solution.solution(new String[] {"I 4", "I 3", "I 2", "I 1", "D 1", "D 1", "D -1", "D -1", "I 5", "I 6"});
+		solution.solution("4177252841",4);
 	}
 	
 }

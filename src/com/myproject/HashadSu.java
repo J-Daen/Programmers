53df332572b5f82a94ce6b8ece4b35a2d3e54f24
J-Daen <jdi2040@naver.com>
2022-08-25 19:37:21 +0900

    하샤드 수

---
+++ src/com/myproject/HashadSu.java
@@ -0,0 +1,24 @@
+package com.myproject;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.Set;
+//하샤드 수
+public class HashadSu {
+	 public boolean solution(int x) {
+        boolean answer = true;
+        String sNumber = String.valueOf(x);
+        int sum = 0;
+        for(char cNumber : sNumber.toCharArray()) {
+        	sum += Integer.valueOf(cNumber);
+        }
+        
+        if(x % sum != 0)
+        	answer = false;
+        
+        return answer;
+    }
+}
